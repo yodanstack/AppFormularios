dc0129566e6cedc4a97e319b0b003a77f591a529
eldann54 <yodanstack@gmail.com>
2024-11-12 19:52:13 -0600

    Actualizacion

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit{
 
+  public myForm: FormGroup = this.fb.group({
+    gender:['M', Validators.required],
+    wanNotification: [true, Validators.required],
+    termnAndConditions: [false, Validators.required]
+  });
+
+  public person = {
+    gender: 'F',
+    wanNotification: 'false',
+  }
+
+constructor( private fb: FormBuilder ){}
+  ngOnInit(): void {
+    this.myForm.reset(this.person)
+  }
+
+isValidField( field: string ): boolean | null {
+  return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+}
+
+onSave(){
+  if(this.myForm.invalid) {
+    this.myForm.markAsTouched()
+    return;
+  }
+  console.log(this.myForm.value)
+
+}
 }
